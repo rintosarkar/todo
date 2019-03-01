929204e20e2bbfdaeec8796a60e3c91baaa14f94
rintosarkar <rinto.sp@gmail.com>
2019-03-02 01:57:22 +0900

    todo app v1.00

---
+++ src/components/AddTodo.vue
@@ -0,0 +1,53 @@
+<template>
+	<div>
+		<form @submit.prevent="addTodo">
+			<input v-model="title" type="text" name="title" placeholder="Add todo ....">
+			<input type="submit" value="Submit" class="btn">
+		</form>
+	</div>
+</template>
+
+
+<script>
+	// import uuid from 'uuid'
+	import axios from 'axios'
+
+	
+	export default{
+		name:"AddTodo",
+		data(){
+			return{
+				title:''
+			}
+		},
+		methods:{
+			addTodo(){
+				const newTodo = {
+					// id: uuid.v4(),
+					title: this.title,
+					completed:false
+				}
+
+				//send up to parent
+
+				this.$emit('add-todo', newTodo)
+				
+				this.title=''
+			}
+		}
+	}
+</script>
+<style scoped>
+	form{
+		display: flex;
+	}
+	input[type="text"]{
+		flex: 10;
+		padding: 5px;
+	}
+	input[type="submit"]{
+		flex:2;
+	}
+
+</style>
+
